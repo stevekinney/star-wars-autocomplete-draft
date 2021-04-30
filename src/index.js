d9d4117ba5799648b85cce58e4df4324dd2229a6
Steve Kinney <hello@stevekinney.net>
2021-04-30 06:01:31 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+import { createEpicMiddleware } from 'redux-observable';
+import rootEpic from './fetch-character-epic';
+
+import reducer from './reducer';
+
+import FetchCharacters from './FetchCharacters';
+import Characters from './Characters';
+
+import './styles.scss';
+
+const epicMiddleware = createEpicMiddleware();
+
+const store = createStore(reducer, applyMiddleware(epicMiddleware));
+
+epicMiddleware.run(rootEpic);
+
+const Application = () => {
+  return (
+    <div className="Application">
+      <h1>Star Wars Autocomplete</h1>
+      <FetchCharacters />
+      <Characters />
+    </div>
+  );
+};
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Application />
+  </Provider>,
+  rootElement,
+);
---
+++ src/reducer.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux';
+import { FETCH_CHARACTERS_FULFILLED } from './actions';
+
+const charactersReducer = (characters = [], action) => {
+    if (action.type === FETCH_CHARACTERS_FULFILLED) {
+        return action.payload;
+    }
+
+    return characters;
+};
+
+export default combineReducers({
+  characters: charactersReducer,
+});
